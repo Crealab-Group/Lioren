54fe09663311eab9325fffacd23671d40bbc1b2d
Juan Carlos <juancarlosf.saavedrap@gmail.com>
2022-04-22 12:40:31 -0400

    Update LiorenInvoiceAuthorizationTest.php

--- tests/LiorenInvoiceAuthorizationTest.php
+++ tests/LiorenInvoiceAuthorizationTest.php
@@ -10,8 +10,18 @@ class LiorenInvoiceAuthorizationTest extends TestCase{
         $this->lioren = Lioren::authenticate( $_ENV['LIOREN_TOKEN']  );
     }
     
-    public function testGetInvoices(){
-        $cafs = $this->lioren->getInvoices();
-        $this->assertInstanceOf(Invoice::class, $cafs);
+    public function testInvoice(){
+        $invoice = $this->lioren->invoice();
+        $this->assertInstanceOf(Invoice::class, $invoice);
+    }
+
+    public function testRequestCAF(){
+        $rCAF = $this->lioren->requestCAF();
+        $this->assertInstanceOf(CAF::class, $rCAF);
+    }
+
+    public function testCAF(){
+        $caf = $this->lioren->CAF();
+        $this->assertInstanceOf(CAF::class, $caf);
     }
 }
